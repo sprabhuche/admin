10083eb88e441f18cc1d31a43a86e42bd4f0d863
Shanmugasundaram <PShanmugasun@ad.harman.com>
2018-12-31 15:51:18 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { LeftnavComponent } from './leftnav/leftnav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent,
+    LeftnavComponent
   ],
   imports: [
     BrowserModule
